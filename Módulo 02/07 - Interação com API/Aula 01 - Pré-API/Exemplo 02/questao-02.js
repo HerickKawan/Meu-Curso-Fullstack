23ee2834a347514af5b862b24a0cdd5088d68f19
Kawan Sena <herickkawansilva@gmail.com>
2024-02-05 17:25:42 -0300

    aula 03

--- Módulo 02/07 - Interação com API/Aula 01 - Pré-API/Exemplo 02/questao-02.js
+++ Módulo 02/07 - Interação com API/Aula 01 - Pré-API/Exemplo 02/questao-02.js
@@ -1,9 +1,18 @@
 
 
-function teste(x,y){
-    //return "oi"
+/*function teste(x,y){
+   // return "oi"
     return x + 5 - y
 }
 
 let senai = teste(3,4)
-alert(senai)
+alert(senai)*/
+
+//alert(somar("ola ",5))
+
+function somar(){
+    return 2 + 4
+}
+
+alert(somar())
+console.log(somar())
--- Módulo 02/07 - Interação com API/Aula 03/Exemplo 02/exemplo-02.js
+++ Módulo 02/07 - Interação com API/Aula 03/Exemplo 02/exemplo-02.js
@@ -1,10 +1,6 @@
 async function obter(){
-    let resposta = await fetch("pizza.txt")
+    let resposta = await fetch("text.txt")
     let convertido = await resposta.text()
     alert(convertido)
-    console.log(convertido)
 }
-
-
-obter()   
-
+obter()
