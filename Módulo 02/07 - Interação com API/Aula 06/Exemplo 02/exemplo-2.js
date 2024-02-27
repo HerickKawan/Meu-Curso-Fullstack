11148c00ed82a3594dd29f7a2c375d71b4f1daf9
HerickKawan <herickkawansilva@gmail.com>
2024-02-27 17:31:52 -0300

    ...

--- Módulo 02/07 - Interação com API/Aula 06/Exemplo 02/exemplo-2.js
+++ Módulo 02/07 - Interação com API/Aula 06/Exemplo 02/exemplo-2.js
@@ -2,14 +2,20 @@ async function celulares(){
     let resultado = await fetch("celular.json")
     let celular = await resultado.json()
 
+    let grupoDiv = document.getElementById("lista-card")
+    //Essa técnica é útil quando você precisa manipular ou interagir com elementos HTML específicos em uma página da web. 
+    //A partir deste ponto, você pode utilizar a variável grupoDiv para realizar várias operações no elemento HTML, 
+    //como adicionar conteúdo, alterar estilos, ouvindo eventos, entre outras manipulações.
+
     for(let x in celular){
     document.body.innerHTML +=`
-    <h1> ${celular[x].t} </h1>
-    <p>A marca do celular é ${celular[x].marca} </p>
-    <p>O modelo do celular é ${celular[x].modelo}</p>
-    <p>Tem ${celular[x].armazenamento} de  aramazenamento</p>
-    <p>Tem ${celular[x].ram} de memória</p>
-   
+    <div class = "card">
+        <h1> ${celular[x].t} </h1>
+        <p>A marca do celular é ${celular[x].marca} </p>
+        <p>O modelo do celular é ${celular[x].modelo}</p>
+        <p>Tem ${celular[x].armazenamento} de  aramazenamento</p>
+        <p>Tem ${celular[x].ram} de memória</p>
+   </div>
     `}
 }
 celulares()
