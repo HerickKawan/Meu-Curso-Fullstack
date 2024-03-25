27281b15ed9d70a5f2c5f15fe9ee5603ae0e32dd
Herick Kawan <herickkawansilva@gmail.com>
2024-03-25 14:23:56 -0300

    aula 06 exemplo 03 copy

--- Módulo 02/07 - Interação com API/Aula 06/Exemplo 03 copy/detalhes.js
+++ Módulo 02/07 - Interação com API/Aula 06/Exemplo 03 copy/detalhes.js
@@ -2,9 +2,33 @@ async function buscarDetalhes(){
     let busca = await fetch("lista.json")
     let produtos = await busca.json()
 
+    // Criou um objeto URLSearchParams e passou
+    // a coleta dos parametros da URL nele.
     let parametros = new URLSearchParams(window.location.search)
+    // Usou o for para percorrer toda a lista de parametros JSON
     let parametroID = parametros.get("produto-id")
-    alert(parametroID)
+
+    // Criou uma variável vazia 
+    let indiceProd 
+    // Usou o for para percorrer toda a lista de parametros JSO
+     for(let x in produtos){
+        //Verifica se o ID de cada produto é igual ao ID
+        // coletado na URL da página no navegador
+        if(produtos[x].id == parametroID){
+            // Atribui á variável vazia, o valor de X, que contém
+            // o indice do produto de corresponde ao ID da URL
+            indiceProd = x
+        }
+    }
+
+    // Adiciona na TAG BODY do HTML, um código HTML cocatenado
+    // com valores do objeto produto encontrado
+    document.body.innerHTML = `
+        <h3>${produtos[indiceProd].nome}</h3>
+        <img src="${produtos[indiceProd.img]}">
+    
+    `
+
 }
 
 buscarDetalhes()
